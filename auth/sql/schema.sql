93a79aa514fe8502ff9298c908102be75d577667
Dinesh Kumar <dinesh@softsuave.com>
2020-06-02 20:33:30 +0530

    [Add,Update][auth,calendar]Added doctor login,patient login,account,roles,appointment,appointment_cancel_reshedule,interval,interval_days,payment_details,work_schedule,wo_schedule_config  tables

--- auth/sql/schema.sql
+++ auth/sql/schema.sql
@@ -27,3 +27,74 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.users
     OWNER to postgres;
+
+CREATE TABLE public.doctor_login
+(
+    doctor_id serial NOT NULL,
+    email character varying(100) NOT NULL,
+    password character varying(100) NOT NULL,
+    account_key character varying(200) NOT NULL,
+    CONSTRAINT doctor_id PRIMARY KEY (doctor_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.doctor_login
+    OWNER to postgres;
+
+CREATE TABLE public.patient_login
+(
+    patient_id serial NOT NULL,
+    email character varying(100) NOT NULL,
+    password character varying(100) NOT NULL,
+    CONSTRAINT patient_id PRIMARY KEY (patient_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.patient_login
+    OWNER to postgres;
+
+
+CREATE TABLE public.roles
+(
+    roles_id serial NOT NULL,
+    doctor_id bigint NOT NULL,
+    roles character varying(100) NOT NULL,
+    account_key character varying(200) NOT NULL,
+    CONSTRAINT roles_id PRIMARY KEY (roles_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.roles
+    OWNER to postgres;
+
+CREATE TABLE public.account
+(
+    account_id serial NOT NULL,
+    email character varying(100) NOT NULL,
+    no_of_users bigint NOT NULL,
+    sub_start_date date NOT NULL,
+    sub_end_date date NOT NULL,
+    account_key character varying(200) NOT NULL,
+    CONSTRAINT account_id PRIMARY KEY (account_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.account
+    OWNER to postgres;
+
+ALTER TABLE public.roles
+    ADD CONSTRAINT roles_to_doctor_id FOREIGN KEY (doctor_id)
+    REFERENCES public.doctor_login (doctor_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+CREATE INDEX fki_roles_to_doctor_id
+    ON public.roles(doctor_id);
--- calendar/sql/schema.sql
+++ calendar/sql/schema.sql
@@ -26,3 +26,138 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.appointment
     OWNER to postgres;
+
+ALTER TABLE public.appointment
+    ADD COLUMN payment_status boolean;
+
+CREATE TABLE public.work_schedule_config
+(
+    wrk_sched_con_id serial NOT NULL,
+    doctor_id bigint NOT NULL,
+    day_of_week character varying(50) NOT NULL,
+    CONSTRAINT wrk_sched_con_id PRIMARY KEY (wrk_sched_con_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.work_schedule_config
+    OWNER to postgres;
+
+CREATE TABLE public.work_schedule
+(
+    wrk_sched_id serial NOT NULL,
+    doctor_id bigint NOT NULL,
+    date date NOT NULL,
+    CONSTRAINT wrk_sched_id PRIMARY KEY (wrk_sched_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.work_schedule
+    OWNER to postgres;
+
+CREATE TABLE public.interval
+(
+    interval_id serial NOT NULL,
+    start_time time without time zone NOT NULL,
+    end_time time without time zone NOT NULL,
+    wrk_sched_con_id bigint NOT NULL,
+    CONSTRAINT interval_id PRIMARY KEY (interval_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.interval
+    OWNER to postgres;
+
+
+CREATE TABLE public.interval_days
+(
+    interval_days_id serial NOT NULL,
+    start_time time without time zone NOT NULL,
+    end_time time without time zone NOT NULL,
+    wrk_sched_id bigint NOT NULL,
+    CONSTRAINT interval_days_id PRIMARY KEY (interval_days_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.interval_days
+    OWNER to postgres;
+
+
+CREATE TABLE public.payment_details
+(
+    payment_id serial NOT NULL,
+    appointment_id bigint NOT NULL,
+    refund bigint NOT NULL,
+    CONSTRAINT payment_id PRIMARY KEY (payment_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.payment_details
+    OWNER to postgres;
+
+CREATE TABLE public.appointment_cancel_reschedule
+(
+    appointment_cancel_reschedule_id serial NOT NULL,
+    cancel_on date NOT NULL,
+    cancel_by bigint NOT NULL,
+    cancel_payment_status character varying(100) NOT NULL,
+    cancel_by_id bigint NOT NULL,
+    reschedule boolean NOT NULL,
+    reschedule_appointment_id bigint NOT NULL,
+    appointment_id bigint NOT NULL,
+    PRIMARY KEY (appointment_cancel_reschedule_id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.appointment_cancel_reschedule
+    OWNER to postgres;
+
+
+ALTER TABLE public.appointment_cancel_reschedule
+    ADD CONSTRAINT can_resch_to_app_id FOREIGN KEY (appointment_id)
+    REFERENCES public.appointment (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+CREATE INDEX fki_can_resch_to_app_id
+    ON public.appointment_cancel_reschedule(appointment_id);
+
+
+ALTER TABLE public.payment_details
+    ADD CONSTRAINT payment_to_app_id FOREIGN KEY (appointment_id)
+    REFERENCES public.appointment (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+CREATE INDEX fki_payment_to_app_id
+    ON public.payment_details(appointment_id);
+
+
+ALTER TABLE public.interval_days
+    ADD CONSTRAINT int_days_to_wrk_sched_id FOREIGN KEY (wrk_sched_id)
+    REFERENCES public.work_schedule (wrk_sched_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+CREATE INDEX fki_int_days_to_wrk_sched_id
+    ON public.interval_days(wrk_sched_id);
+
+ALTER TABLE public.interval
+    ADD CONSTRAINT interval_to_wrk_sched_config_id FOREIGN KEY (wrk_sched_con_id)
+    REFERENCES public.work_schedule_config (wrk_sched_con_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+CREATE INDEX fki_interval_to_wrk_sched_config_id
+    ON public.interval(wrk_sched_con_id);
