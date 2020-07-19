bc0abc6f0518c5b1589bf5a5cda584ca06f672d6
Prabhagar <prabhagar@softsuave.com>
2020-07-20 03:05:08 +0530

    updaet on appointmentSlots view api

--- calendar/src/utility/helper.ts
+++ calendar/src/utility/helper.ts
@@ -6,21 +6,31 @@ export class Helper {
         return startTimeMilliSeconds;
     }
 
+
     static getConsultationTimeInMilliSeconds(time: any) {
-        let startTimeMilliSeconds =(time * 60000);
+        let startTimeMilliSeconds = (time * 60000);
         return startTimeMilliSeconds;
     }
 
+    static getMilliSecondsToTimeFormat(time: any) {
+        let sec = time % 60;
+        let min = (time / 60) % 60;
+        let hr = (time / (60 * 60)) % 24;
+        let result = hr + ":" + min + ":" + sec;
+        return result;
+    }
+
+
     static getTimeinHrsMins(milliseconds: any) {
         //Get hours from milliseconds
-        var hours = milliseconds / (1000*60*60);
+        var hours = milliseconds / (1000 * 60 * 60);
         var absoluteHours = Math.floor(hours);
         var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
 
         //Get remainder from hours and convert to minutes
         var minutes = (hours - absoluteHours) * 60;
         var absoluteMinutes = Math.floor(minutes);
-        var m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
+        var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
 
         //Get remainder from minutes and convert to seconds
         var seconds = (minutes - absoluteMinutes) * 60;
@@ -28,12 +38,20 @@ export class Helper {
         var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
 
         return h + ':' + m + ':' + s;
-     }
-    
-     static getMinInMilliSeconds(x: any) {
-         let mi = x * 60000
-          return (mi)
-      }
+    }
 
+    static getMinInMilliSeconds(x: any) {
+        let mi = x * 60000
+        return (mi)
+    }
+
+
+    static getDayMonthYearFromDate(date: any) {
+        let dt = new Date(date);
+        let year = dt.getFullYear();
+        let month = dt.getMonth();
+        let day = dt.getDate();
+        return year + ':' + month + ':' + day;
+    }
 
 }
