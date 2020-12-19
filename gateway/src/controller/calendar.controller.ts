01ceaab3cc04571a4119c39e0ee9b54555d53df2
Ponsundaram <ponsundaram@softsuave.com>
2020-12-19 11:51:22 +0530

    [Upload][PDF] Uploaded pdf in prescription

--- gateway/src/controller/calendar.controller.ts
+++ gateway/src/controller/calendar.controller.ts
@@ -1331,22 +1331,4 @@ export class CalendarController {
         
     }
 
-    @Post('patient/prescription/download')
-    @ApiBearerAuth('JWT')
-    @UseGuards(AuthGuard())
-    @ApiTags('Patient')
-    @ApiBody({ type: PrescriptionDto })
-    @ApiOkResponse({ description: 'request body example:   {"appointmentId": "251"}' })
-    @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
-    async prescriptionDownload(@patient() check: boolean, @Request() req, @Body() prescriptionDto: PrescriptionDto) {
-        if (!check)
-            return { statusCode: HttpStatus.BAD_REQUEST, message: CONSTANT_MSG.NO_PERMISSION }
-        if (req.user.role == CONSTANT_MSG.ROLES.PATIENT) {
-            await this.calendarService.updatePatLastActive(req.user.patientId);
-        }
-        this.logger.log(`Doctor prescriptionDownload  Api -> Request data ${JSON.stringify(prescriptionDto)}`);
-        return await this.calendarService.prescriptionDownload(req.user, prescriptionDto.appointmentId);
-    }
-
-
 }
