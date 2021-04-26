1241317675fd8fe5ac1c57daa23bf1fd1413979f
virujh <techadmin@virujh.com>
2021-04-26 18:00:04 +0530

    Log n local setup fixes

--- gateway/src/gateway/video.gateway.ts
+++ gateway/src/gateway/video.gateway.ts
@@ -36,7 +36,7 @@ export class VideoGateway {
   async createTokenForDoctor(client: AuthenticatedSocket, data : string) {
     this.logger.log(`Socket request for create Token for Doctor from Doc-key => ${client.auth.data.doctor_key}`);
     const response : any = await this.videoService.videoDoctorSessionCreate(client.auth.data.doctor_key);
-    console.log("response Doctor >>" + JSON.stringify(response));
+    this.logger.log("response Doctor >>" + JSON.stringify(response));
     client.emit("videoTokenForDoctor", response);
     return response;
   }
@@ -45,7 +45,7 @@ export class VideoGateway {
   async createTokenForPatientByDoctor(client: AuthenticatedSocket, appointmentId : string) {
     this.logger.log(`Socket request for create Token for Patient from Doc-key => ${client.auth.data.doctor_key} and appointmentId => ${appointmentId}` );
     const response : any = await this.videoService.createTokenForPatientByDoctor(client.auth.data.doctor_key, appointmentId);
-    console.log("response Patient >>" + JSON.stringify(response));
+    this.logger.log("response Patient >>" + JSON.stringify(response));
     let patientSocketList : Socket[] = this.socketStateService.get("CUSTOMER_"+response.patient);
     patientSocketList.forEach( (val : Socket) => {
       val.emit("videoTokenForPatient", response);
@@ -63,7 +63,7 @@ export class VideoGateway {
   async removePatientTokenByDoctor(client: AuthenticatedSocket, data : any) {
     this.logger.log(`Socket request remove Patient Token By Doctor from Doc-key => ${client.auth.data.doctor_key} and appointmentId => ${data}` );
     const response:any = await this.videoService.removePatientTokenByDoctor(client.auth.data.doctor_key, data.appointmentId, data.status);
-    console.log("response >>" + JSON.stringify(response));
+    this.logger.log("response >>" + JSON.stringify(response));
     let patientSocketList : Socket[] = this.socketStateService.get("CUSTOMER_"+response.patient);
     patientSocketList.forEach( (val : Socket) => {
       val.emit("videoTokenRemoved", {...response, callEndStatus: data.status, appointmentId: data.appointmentId});
@@ -76,7 +76,7 @@ export class VideoGateway {
   async removeSessionAndTokenByDoctor(client: AuthenticatedSocket, appointmentId : string) {
     this.logger.log(`Socket request remove Session And Token By Doctor from Doc-key => ${client.auth.data.doctor_key}` );
     const response:any = await this.videoService.removeSessionAndTokenByDoctor(client.auth.data.doctor_key,appointmentId);
-    console.log("response >>" + JSON.stringify(response));
+    this.logger.log("response >>" + JSON.stringify(response));
     let patientSocketList : Socket[] = this.socketStateService.get("CUSTOMER_"+response.patient);
     patientSocketList.forEach( (val : Socket) => {
       val.emit("videoSessionRemoved", response);
@@ -101,11 +101,11 @@ export class VideoGateway {
       const patientTodayApp : any = await this.videoService.patientUpcomingAppointments(userInfo.patientId, 0, 0);
       let doctorArr = [0];
 
-      console.log('patientTodayApp = > ', patientTodayApp);
+      this.logger.log('patientTodayApp = > ', patientTodayApp);
 
       patientTodayApp.forEach(element => {
-      console.log(element);
-        console.log('doctor = >', element.doctorId);
+      this.logger.log(element);
+        this.logger.log('doctor = >', element.doctorId);
         if (element.doctorId && (doctorArr.length && !doctorArr.includes(element.doctodId))) {
           doctorArr.push(element.doctorId);
           // docList -> DOCTOR
@@ -132,7 +132,7 @@ export class VideoGateway {
 
     this.logger.log(`Socket request to update consultationStatus By Doctor from Doc-key => ${client.auth.data.doctor_key}${data.appointmentId}`);
     const response: any = await this.videoService.updateConsultationStatus(client.auth.data.doctor_key, data.appointmentId);
-    console.log("response >>" + JSON.stringify(response));
+    this.logger.log("response >>" + JSON.stringify(response));
 
     // After successfull updatation emit to all patient to block appointment details change
     if (response && response.statusCode === 200) {
